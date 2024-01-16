d660c61e9151ac78ded9fa4d7c6e816686535a88
Wh1teMage <I1cecrush@yandex.ru>
2024-01-16 19:01:09 +0300

    972

--- src/client/static/EffectsStatic.ts
+++ src/client/static/EffectsStatic.ts
@@ -5,19 +5,19 @@ import { EffectsUtilities } from "shared/utility/EffectsUtilities"
 
 const player = Players.LocalPlayer
 
-export const PlayEffect = (name: EffectName, additional?: Map<string, any>) => {
+export const PlayEffect = (name: string, additional?: Map<string, any>) => {
     if (!Effects.get(name)) { warn('Effects Doesnt Exist!'); return }
     Effects.get(name)!(additional)
 }
 
-export const Effects = new Map<EffectName, (
+export const Effects = new Map<string, (
     additional?: Map<string, any>
 ) => void>([])
 
-Effects.set(EffectName.ReplicatePurchase, (additional) => {
+Effects.set('ReplicatePurchase', (additional) => {
     Events.PurchasePrompt.fire(additional?.get('productId') as number, player.UserId)
 })
 
-Effects.set(EffectName.ClickSound, () => {
+Effects.set('ClickSound', () => {
     EffectsUtilities.PlaySound('rbxassetid://7741939171')
 })
