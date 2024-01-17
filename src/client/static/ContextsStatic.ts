3c4340a5aff8e6f1fcc23c33006625c116f3fb6d
AGRIG <64426613+AGRIG05@users.noreply.github.com>
2024-01-17 20:22:14 +0300

    clickeffect

--- src/client/static/ContextsStatic.ts
+++ src/client/static/ContextsStatic.ts
@@ -1,12 +1,24 @@
 import { Players } from "@rbxts/services"
 import { PlayerController } from "client/controllers/PlayerController"
 import { Events } from "client/network"
+import { PlayEffect } from "./EffectsStatic"
 
 const player = Players.LocalPlayer
 
-export const Contexts = new Map<string, {obj: Enum.KeyCode | Enum.UserInputType | Enum.PlayerActions | string, state?: Enum.UserInputState, callback: () => void}>
+export const Contexts = new Map<string, {obj: Enum.KeyCode | Enum.UserInputType | Enum.PlayerActions | string, state?: Enum.UserInputState, callback: () => void}>()
 
 Contexts.set('test', {obj: Enum.KeyCode.R, state: Enum.UserInputState.Begin, callback: () => {
     print('Send Input')
     Events.RegisterInput.fire('test', Enum.UserInputState.Begin)
 }})
+
+Contexts.set('ClickEffect', {obj: Enum.UserInputType.MouseButton1, state: Enum.UserInputState.Begin, callback: () => {
+    let mouse = player.GetMouse()
+    PlayEffect('ClickSound')
+    PlayEffect('ClickEffect', new Map<string, any>([['position', new Vector2(mouse.X, mouse.Y)]]))
+}})
+
+// Contexts.set('click effect', {obj: Enum.UserInputType.Touch, state: Enum.UserInputState.Begin, callback: () => {
+//     print('Send Input')
+//     Events.RegisterInput.fire('test', Enum.UserInputState.Begin)
+// }})
